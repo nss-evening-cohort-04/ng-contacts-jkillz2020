ab13724d1a16ab845c4d6ebe5efeaf7c01a94582
jkillz2020 <kilgore.josh@gmail.com>
2016-11-17 21:02:36 -0600

    changed styling, got objects from fb, show hide buttons

--- javascripts/controllers/ContactsCtrl.js
+++ javascripts/controllers/ContactsCtrl.js
@@ -1,1 +1,33 @@
 "use strict";
+
+app.controller('ContactsCtrl', function($scope, AddressFactory){
+  $scope.welcome = "hello"
+  $scope.showContactsView = true;
+  $scope.newContact = {};
+  $scope.contacts = [];
+
+  let getContacts = function(){
+AddressFactory.getContactList().then(function(fbContacts){
+      $scope.contacts=fbContacts;
+    })
+  }
+
+getContacts();
+
+ $scope.allContacts = function(){
+  console.log("you clicked show all contacts");
+  $scope.showContactsView = true;
+ }
+ $scope.newContact = function(){
+  console.log("you clicked add new contact");
+  $scope.showContactsView = false;
+ }
+
+ $scope.addNewContact = function(){
+  AddressFactory.postNewContact($scope.newContact).then(function(contactId){
+  getContacts();
+  $scope.newContact = {};
+  $scope.showContactsView = true;
+  })
+ }
+})
--- javascripts/factories/AddressFactory.js
+++ javascripts/factories/AddressFactory.js
@@ -1,1 +1,50 @@
-"use strict"
+"use strict";
+
+app.factory('AddressFactory', function($q, $http, FIREBASE_CONFIG){
+
+  var getContactList=function(){
+    return $q((resolve, reject)=>{
+      $http.get(`${FIREBASE_CONFIG.databaseURL}/Contacts.json`)
+        .success(function(response){
+          console.log("response", response)
+          let contacts =[];
+          Object.keys(response).forEach(function(key){
+            response[key].id=key;
+            contacts.push(response[key]);
+          });
+          resolve(contacts);
+        })
+        .error(function(errorResponse){
+          reject(errorResponse);
+        })
+    })
+  }
+
+  var postNewContact = function(){
+    return $q((resolve, reject)=>{
+      $http.post(`${FIREBASE_CONFIG.databaseURL}/Contacts.json`,
+        JSON.stringify({
+          firstName: newContact.firstName,
+          lastName: newContact.lastName,
+          streetAddress: newContact.streetAddress,
+          city: newContact.city,
+          state: newContact.state,
+          zipcode: newContact.zipcode,
+          phone: newContact.phone,
+          email: newContact.email,
+        })
+      )
+        .success(function(postResponse){
+          resolve(postResponse);
+        })
+        .error(function(postError){
+          reject(postError);
+        })
+    })
+  }
+
+
+  return {getContactList:getContactList,
+          postNewContact:postNewContact}
+     
+});
